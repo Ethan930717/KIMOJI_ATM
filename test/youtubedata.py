4ddaa7d52467f3f3068a5641415d298ba9d2a8d4
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-09 17:26:14 +0800

    Update youtubedata.py

--- test/youtubedata.py
+++ test/youtubedata.py
@@ -6,17 +6,26 @@ youtube = build('youtube', 'v3', developerKey='AIzaSyDKvGQR18CSwDfjauXyt8-HmCrAt
 # 用你的播放列表ID替换这里的PLAYLIST_ID
 playlist_id = 'PLOrf2h5ONlwW9lFJ-68wtf7FdMG-45FNG'
 
-# 获取播放列表中的视频
-request = youtube.playlistItems().list(
-    part='snippet',
-    playlistId=playlist_id,
-    maxResults=50  # API一次最多返回50个结果
-)
-response = request.execute()
+def get_playlist_videos(playlist_id):
+    request = youtube.playlistItems().list(
+        part='snippet',
+        playlistId=playlist_id,
+        maxResults=50  # API一次最多返回50个结果
+    )
+    video_number = 1
 
-# 打印视频标题和链接
-for item in response.get('items', []):
-    title = item['snippet']['title']
-    video_id = item['snippet']['resourceId']['videoId']
-    video_url = f'https://www.youtube.com/watch?v={video_id}'
-    print(f'{title}: {video_url}')
+    while request is not None:
+        response = request.execute()
+
+        # 打印视频标题和链接
+        for item in response.get('items', []):
+            title = item['snippet']['title']
+            video_id = item['snippet']['resourceId']['videoId']
+            video_url = f'https://www.youtube.com/watch?v={video_id}'
+            print(f'{video_number}. {title}: {video_url}')
+            video_number += 1
+
+        # 获取下一页
+        request = youtube.playlistItems().list_next(request, response)
+
+get_playlist_videos(playlist_id)
