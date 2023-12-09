bdb395444535978648c31a9283e07fd8b2256923
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-09 13:31:38 +0800

    Update tm.py

--- tm.py
+++ tm.py
@@ -75,6 +75,5 @@ def rename_folder(folder_path):
     else:
         print("没有找到匹配的 TMDB 资源。")
 
-
-# 示例
-rename_folder("/Users/Ethan/Desktop/河西走廊")
+folder = input("请指定需要更名的文件夹路径")
+rename_folder(folder)
